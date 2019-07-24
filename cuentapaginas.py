bf788fc700106dda46e190a82faf0c078904d792
LandaburoSantiago <53238054+LandaburoSantiago@users.noreply.github.com>
2019-07-24 09:12:54 -0300

    Update cuentapaginas.py

--- cuentapaginas.py
+++ cuentapaginas.py
@@ -7,8 +7,3 @@ rxcountpages = re.compile(r"/Type\s*/Page([^s]|$)", re.MULTILINE | re.DOTALL)
 def count_pages(filename):
     data = file(filename, "r+b").read()
     return len(rxcountpages.findall(data))
-
-
-if __name__ == "__main__":
-    filename = '/media/santiago/DATOS_LINUX/Pagina fotocopias/Mas de polimorfismo y herencia.pdf'
-    print count_pages(filename)
