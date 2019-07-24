67ba19d997873c7277705d009f3a9e426f2f316b
LandaburoSantiago <53238054+LandaburoSantiago@users.noreply.github.com>
2019-07-24 19:34:50 -0300

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from datetime import datetime, date, timedelta
 import funciones_bbdd
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = '/Archivos_PDF'
+app.config['UPLOAD_FOLDER'] = '/static/Archivos_PDF'
 directorio = os.path.dirname(__file__)
 
 
