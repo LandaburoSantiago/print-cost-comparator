096e894d54805cb001997f1d3d0c9e26ca7566b6
LandaburoSantiago <53238054+LandaburoSantiago@users.noreply.github.com>
2019-07-25 10:50:26 -0300

    Delete cuentapaginas.py

--- cuentapaginas.py
+++
@@ -1,9 +0,0 @@
-import re
-import os
-
-rxcountpages = re.compile(r"/Type\s*/Page([^s]|$)", re.MULTILINE | re.DOTALL)
-
-
-def count_pages(filename):
-    data = file(filename, "r+b").read()
-    return len(rxcountpages.findall(data))
