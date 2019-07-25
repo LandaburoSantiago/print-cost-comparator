8503b62002243b65a159dddb90b73e2acfcc6370
LandaburoSantiago <53238054+LandaburoSantiago@users.noreply.github.com>
2019-07-25 10:58:14 -0300

    Update cuentapaginas.py

--- cuentapaginas.py
+++ cuentapaginas.py
@@ -3,7 +3,6 @@ import PyPDF2
 
 def contar_paginas(directorio, nombre_archivo):
     ruta_archivo = directorio+'/'+nombre_archivo
-    archivo = open(ruta_archivo)
-    leer_pdf = PyPDF2.PdfFileReader(archivo)
+    leer_pdf = PyPDF2.PdfFileReader(open(ruta_archivo, mode='rb'))
     numero_de_paginas = leer_pdf.getNumPages()
     return numero_de_paginas
