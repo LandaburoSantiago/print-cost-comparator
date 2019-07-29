d6d2c2750dce6579ddb5e590dfef8d126e1f84f6
LandaburoSantiago <53238054+LandaburoSantiago@users.noreply.github.com>
2019-07-29 10:37:34 -0300

    Update funciones_bbdd.py

--- funciones_bbdd.py
+++ funciones_bbdd.py
@@ -47,6 +47,7 @@ def listar_color(cantidad_paginas, simple_doble, ciudad, momento_dia, hora, dia)
     db = conexion()
     documento = db.get_collection('fotocopiadoras_'+ciudad).find()
     lista = []
+    int(dia)
     if simple_doble == 'doblefaz':
         # AL SER DOBLE FAZ SE DIVIDE LA CANTIDAD DE PAGINAS POR 2
         if (cantidad_paginas % 2 == 0):
@@ -168,7 +169,7 @@ def listar_byn(cantidad_paginas, simple_doble, ciudad, momento_dia, hora, dia):
     db = conexion()
     documento = db.get_collection('fotocopiadoras_'+ciudad).find()
     lista = []
-    print(dia+'!!!!!')
+    int(dia)
     # AL SER DOBLE FAZ SE DIVIDE LA CANTIDAD DE PAGINAS POR 2
     if simple_doble == 'doblefaz':
         if (cantidad_paginas % 2 == 0):
