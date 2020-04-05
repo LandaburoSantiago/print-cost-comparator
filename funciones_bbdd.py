7e06f9b5530088981c920b84c30d2c3ebdfe043f
Santiago Landaburo <santilandaburo73@gmail.com>
2020-04-05 15:49:49 -0300

    correcion en get_one

--- funciones_bbdd.py
+++ funciones_bbdd.py
@@ -286,5 +286,5 @@ def listar_byn(cantidad_paginas, simple_doble, ciudad, momento_dia, hora, dia):
 def get_one(id, ciudad):
     db = conexion()
     documento = db.get("https://proyecto-fotocopiadoras-bae45.firebaseio.com/fotocopiadoras/fotocopiadoras_"+ciudad+"/"+id, "")
-    documento['_id'] = str(documento['_id'])
+    documento = documento[0]
     return documento
